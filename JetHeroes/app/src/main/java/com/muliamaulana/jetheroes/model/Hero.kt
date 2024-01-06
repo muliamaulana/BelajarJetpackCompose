e16a6244c05c5481ed4fcdd9cf156650d478bf1a
muliamaulana <m.muliamaulana@gmail.com>
2024-01-06 11:10:53 +0700

    Studi Kasus Membuat List Pahlawan

---
+++ JetHeroes/app/src/main/java/com/muliamaulana/jetheroes/model/Hero.kt
@@ -0,0 +1,10 @@
+package com.muliamaulana.jetheroes.model
+
+/**
+ * Created by muliamaulana on 05/01/24.
+ */
+data class Hero(
+    val id: String,
+    val name: String,
+    val photoUrl: String
+)
