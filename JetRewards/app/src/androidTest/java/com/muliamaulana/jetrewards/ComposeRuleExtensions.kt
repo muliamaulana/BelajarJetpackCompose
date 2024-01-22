51d6ba63d0cf7cf862676b4de20e937c1738053f
muliamaulana <m.muliamaulana@gmail.com>
2024-01-22 08:23:42 +0700

    Latihan Semantic & Tag Pada UI Testing

---
+++ JetRewards/app/src/androidTest/java/com/muliamaulana/jetrewards/ComposeRuleExtensions.kt
@@ -0,0 +1,16 @@
+package com.muliamaulana.jetrewards
+
+import androidx.activity.ComponentActivity
+import androidx.annotation.StringRes
+import androidx.compose.ui.test.SemanticsNodeInteraction
+import androidx.compose.ui.test.junit4.AndroidComposeTestRule
+import androidx.compose.ui.test.onNodeWithText
+import androidx.test.ext.junit.rules.ActivityScenarioRule
+
+/**
+ * Created by muliamaulana on 22/01/24.
+ */
+
+fun <A : ComponentActivity> AndroidComposeTestRule<ActivityScenarioRule<A>, A>.onNodeWithStringId(
+    @StringRes id: Int,
+): SemanticsNodeInteraction = onNodeWithText(activity.getString(id))
